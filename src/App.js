b27653920d3fc4175cbee6e096c18350daac1821
quan <nikolaszengchunhui@hotmail.com>
2019-03-13 23:47:23 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,32 +1,18 @@
-import React, { Component } from "react";
-import { Switch, Route, Redirect, withRouter } from "react-router-dom";
-import AuthorizedRoute from "./AuthorizedRoute";
-import Login from "./pages/Login/Login";
-import Layout from "./pages/Layout/Layout";
-import NoFound from "./pages/NoFound/NoFound";
-
-const whiteList = ["/login", "/register"];
-
-
-@withRouter
+import React, { Component } from 'react'
+import { app, BrowserView } from 'electron';
 export default class App extends Component {
+  createWindow(){
+  
+  }
+  componentWillMount()
+  {
+
+  }
   render() {
-    /**
-     * 判断当前路径,在/目录的话直接跳转/login, 在白名单whiteList数组内的话不做任何改变
-     */
-    const loginRedirectUrl =
-      whiteList.indexOf(this.props.location.pathname) > -1
-        ? this.props.location.pathname
-        : "/login";
     return (
       <div>
-        <Switch>
-          <Route path="/login" component={Login} />
-          <Redirect from="/" exact to={loginRedirectUrl} />
-          <AuthorizedRoute path="/path" component={Layout} />
-          <Route component={NoFound} />
-        </Switch>
+        
       </div>
-    );
+    )
   }
 }
