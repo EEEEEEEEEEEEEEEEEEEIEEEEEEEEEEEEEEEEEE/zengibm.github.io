f3a5fbcbe064a13d95a18542f7f86abaade281ea
quan <nikolaszengchunhui@hotmail.com>
2019-03-29 08:13:45 +0800

    爬虫1

--- server/crawel/crawel1.js
+++ server/crawel/crawel1.js
@@ -1,16 +1,17 @@
-const request = require('request');
+const fs = require('fs');
 
+const request = require('request');
 class baseCrawel {
   constructor() {
-      this.getProxyList()
+    this.getProxyList();
   }
 
   getProxyList() {
-    const api_url = 'baidu.com';
+    const api_url = 'http://api.hphrj.com/Index/getSystemProduct2';
 
     return new Promise((resolve, reject) => {
       const options = {
-        method: 'GET',
+        method: 'POST',
         url: api_url,
         gzip: true,
         encoding: null,
@@ -21,18 +22,21 @@ class baseCrawel {
           'Accept-Language': 'zh-CN,zh;q=0.8,en;q=0.6,zh-TW;q=0.4',
           'User-Agent':
             'Mozilla/8.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/45.0.2454.101 Safari/537.36',
-          referer: 'http://www.66ip.cn/'
+          referer: 'http://api.hphrj1.com',
+          origin: 'http://api.hphrj1.com'
         }
       };
 
       request(options, (err, res, body) => {
         try {
           if (err) throw err;
-          if (/meta.*charset=gb2312/.test(body)) {
-            body = iconv.decode(body, 'gbk');
-          }
-          var ret = body.match(/\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}:\d{1,4}/g);
-          resolve(ret);
+          fs.writeFile('json.json', body, err => {
+            if (err) {
+            } else {
+              console.log('ok....');
+            }
+          });
+          // console.log(err, res, body);
         } catch (e) {
           return reject(e);
         }
@@ -40,3 +44,10 @@ class baseCrawel {
     });
   }
 }
+
+new baseCrawel();
+// if (/meta.*charset=gb2312/.test(body)) {
+//   body = iconv.decode(body, 'gbk');
+// }
+// var ret = body.match(/\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}:\d{1,4}/g);
+// resolve(ret);
