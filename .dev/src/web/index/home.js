3996ba57630a7972555cdbe8f6f2efe77f16c09d
zat.圈圈 <zengibm@gmail.com>
2019-04-03 23:23:58 +0800

    初步完成首页样式

--- .dev/src/web/index/home.js
+++ .dev/src/web/index/home.js
@@ -4,7 +4,8 @@ export default class home extends Component {
   constructor() {
     super();
     this.state = {
-      contart_list: ["github", "email", "qq"]
+      contart_list: ["github", "email", "qq"],
+      article_list: []
     };
   }
   render() {
@@ -13,12 +14,127 @@ export default class home extends Component {
       <div className="web_home">
         <div className="top">
           <div className="avatar">
-            <img />
+            <img src={require("../../common/img/avatar.jpeg")} />
           </div>
-          {contart_list.map((v, k) => {
-            return <a href="">{v}</a>;
-          })}
+          <h2>圈圈的代码世界</h2>
+          <div className="route_link">
+            {contart_list.map((v, k) => {
+              return <a href="" key={k} />;
+            })}
+          </div>
         </div>
+        <section className="container">
+          <div className="article_list">
+            <ul>
+              <h2 className="list_date">2019-2-10</h2>
+              <li>
+                <div className="content">
+                  <h3>文章标题</h3>
+                  <p>
+                    文章简介 文章简介 文章 文章简介 文章简介 文章 文章简介
+                    文章简介 文章 文章简介 文章简介 文章 文章简介 文章简介 文章
+                    简介 文章简介 文章简介
+                  </p>
+                </div>
+                <span>10:10:30</span>
+              </li>
+              <li>
+                <div className="content">
+                  <h3>文章标题</h3>
+                  <p>
+                    文章简介 文章简介 文章 文章简介 文章简介 文章 文章简介
+                    文章简介 文章 文章简介 文章简介 文章 文章简介 文章简介 文章
+                    简介 文章简介 文章简介
+                  </p>
+                </div>
+                <span>10:10:30</span>
+              </li>
+              <li>
+                <div className="content">
+                  <h3>文章标题</h3>
+                  <p>
+                    文章简介 文章简介 文章 文章简介 文章简介 文章 文章简介
+                    文章简介 文章 文章简介 文章简介 文章 文章简介 文章简介 文章
+                    简介 文章简介 文章简介
+                  </p>
+                </div>
+                <span>10:10:30</span>
+              </li>
+            </ul>
+            <ul>
+              <h2 className="list_date">2019-2-10</h2>
+              <li>
+                <div className="content">
+                  <h3>文章标题</h3>
+                  <p>
+                    文章简介 文章简介 文章 文章简介 文章简介 文章 文章简介
+                    文章简介 文章 文章简介 文章简介 文章 文章简介 文章简介 文章
+                    简介 文章简介 文章简介
+                  </p>
+                </div>
+                <span>10:10:30</span>
+              </li>
+              <li>
+                <div className="content">
+                  <h3>文章标题</h3>
+                  <p>
+                    文章简介 文章简介 文章 文章简介 文章简介 文章 文章简介
+                    文章简介 文章 文章简介 文章简介 文章 文章简介 文章简介 文章
+                    简介 文章简介 文章简介
+                  </p>
+                </div>
+                <span>10:10:30</span>
+              </li>
+              <li>
+                <div className="content">
+                  <h3>文章标题</h3>
+                  <p>
+                    文章简介 文章简介 文章 文章简介 文章简介 文章 文章简介
+                    文章简介 文章 文章简介 文章简介 文章 文章简介 文章简介 文章
+                    简介 文章简介 文章简介
+                  </p>
+                </div>
+                <span>10:10:30</span>
+              </li>
+            </ul>
+            <ul>
+              <h2 className="list_date">2019-2-10</h2>
+              <li>
+                <div className="content">
+                  <h3>文章标题</h3>
+                  <p>
+                    文章简介 文章简介 文章 文章简介 文章简介 文章 文章简介
+                    文章简介 文章 文章简介 文章简介 文章 文章简介 文章简介 文章
+                    简介 文章简介 文章简介
+                  </p>
+                </div>
+                <span>10:10:30</span>
+              </li>
+              <li>
+                <div className="content">
+                  <h3>文章标题</h3>
+                  <p>
+                    文章简介 文章简介 文章 文章简介 文章简介 文章 文章简介
+                    文章简介 文章 文章简介 文章简介 文章 文章简介 文章简介 文章
+                    简介 文章简介 文章简介
+                  </p>
+                </div>
+                <span>10:10:30</span>
+              </li>
+              <li>
+                <div className="content">
+                  <h3>文章标题</h3>
+                  <p>
+                    文章简介 文章简介 文章 文章简介 文章简介 文章 文章简介
+                    文章简介 文章 文章简介 文章简介 文章 文章简介 文章简介 文章
+                    简介 文章简介 文章简介
+                  </p>
+                </div>
+                <span>10:10:30</span>
+              </li>
+            </ul>
+          </div>
+        </section>
       </div>
     );
   }
